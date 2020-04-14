cc2e38c8058de2f2170ae7dfc9a9ea1c8ab5d2b7
Mike Mases <mike.mases.uk@gmail.com>
2020-04-14 13:42:54 +0100

    Converting project to React SPA

---
+++ src/WebApp.UnitTests/Hubs/BoardHub.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace HomeBoard.WebApp.UnitTests.Hubs
+{
+    public class BoardHub : Hub
+    {
+        
+    }
+}
--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -1,6 +1,8 @@
 using HomeBoard.WebApp.Installers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,9 +22,15 @@ namespace HomeBoard.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
             services.ConfigureWeatherService(Configuration);
             services.ConfigureTrainsService(Configuration);
+            services.AddControllersWithViews();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,14 +51,25 @@ namespace HomeBoard.WebApp
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSpaStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
             });
         }
     }
