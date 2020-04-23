d2b7240c050430d4cc396e90edad72a9551b7f9b
Mike Mases <mike.mases.uk@gmail.com>
2020-04-23 13:47:51 +0100

    Hub emits message to client

--- src/WebApp.UnitTests/Hubs/HomeBoardUpdateHubShould.cs
+++ src/WebApp.UnitTests/Hubs/HomeBoardUpdateHubShould.cs
@@ -1,10 +1,53 @@
+using System.Linq;
+using System.Threading.Tasks;
+using HomeBoard.Models;
+using HomeBoard.WebApp.Builders;
+using HomeBoard.WebApp.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
 using NUnit.Framework;
 
-namespace WebApp.UnitTests.Hubs
+namespace HomeBoard.WebApp.UnitTests.Hubs
 {
     [TestFixture]
     public class HomeBoardUpdateHubShould
     {
-        
+        private HomeBoardUpdateHub _hub;
+        private ILogger<HomeBoardUpdateHub> _logger;
+        private IHomeBoardViewModelBuilder _builder;
+        private IClientProxy _clientProxy;
+        private HomeBoardViewModel _model;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _model = new HomeBoardViewModel();
+            _logger = Substitute.For<ILogger<HomeBoardUpdateHub>>();
+            _builder = Substitute.For<IHomeBoardViewModelBuilder>();
+            _builder.BuildViewModel().Returns(_model);
+            _clientProxy = Substitute.For<IClientProxy>();
+            var clients = Substitute.For<IHubCallerClients>();
+            _hub = new HomeBoardUpdateHub(_builder, _logger);
+            _hub.Clients = clients;
+            clients.All.Returns(_clientProxy);
+        }
+
+        [Test]
+        public async Task BuildViewModel()
+        {
+            await _hub.UpdateHomeBoard();
+
+            await _builder.Received(1).BuildViewModel();
+        }
+
+        [Test]
+        public async Task SendMessageToClients()
+        {
+            await _hub.UpdateHomeBoard();
+
+            await _clientProxy.Received(1).SendCoreAsync("homeBoardUpdate", Arg.Is<object[]>(o => o.Contains(_model)));
+        }
+
     }
 }
--- src/WebApp/Hubs/HomeBoardUpdateHub.cs
+++ src/WebApp/Hubs/HomeBoardUpdateHub.cs
@@ -1,21 +1,25 @@
 using System.Threading.Tasks;
 using HomeBoard.WebApp.Builders;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 
-namespace WebApp.Hubs
+namespace HomeBoard.WebApp.Hubs
 {
     public class HomeBoardUpdateHub : Hub
     {
         private readonly IHomeBoardViewModelBuilder _builder;
+        private readonly ILogger<HomeBoardUpdateHub> _logger;
 
-        public HomeBoardUpdateHub(IHomeBoardViewModelBuilder builder)
+        public HomeBoardUpdateHub(IHomeBoardViewModelBuilder builder, ILogger<HomeBoardUpdateHub> logger)
         {
             _builder = builder;
+            _logger = logger;
         }
 
         public async Task UpdateHomeBoard()
         {
-            
+            var viewModel = await _builder.BuildViewModel();
+            await Clients.All.SendAsync("homeBoardUpdate", viewModel);
         }
     }
 }
