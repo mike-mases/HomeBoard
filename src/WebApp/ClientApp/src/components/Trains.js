9cf26eddae0710fe5138e66178dec3e53e1a5060
Mike Mases <mike.mases.uk@gmail.com>
2020-05-04 15:52:40 +0100

    Trialling collapsible table

--- src/WebApp/ClientApp/src/components/Trains.js
+++ src/WebApp/ClientApp/src/components/Trains.js
@@ -5,7 +5,7 @@ import './Trains.css'
 export class Trains extends Component {
     constructor(props) {
         super(props);
-        this.state = { trains: props.trains };
+        this.state = { trains: props.trains, isOpen: false };
     }
 
     componentWillReceiveProps(props) {
@@ -15,11 +15,21 @@ export class Trains extends Component {
     render() {
         const trains = this.state.trains;
 
+        const toggle = () => this.setState({ isOpen: !this.state.isOpen });
+
         return (
             <div>
                 <h2 className="display-3">Trains</h2>
 
-                <Table striped>
+                <Table striped className="fold-table">
+                    <colgroup>
+                        <col style={{ width: 15 + '%' }} />
+                        <col style={{ width: 25 + '%' }} />
+                        <col style={{ width: 15 + '%' }} />
+                        <col style={{ width: 15 + '%' }} />
+                        <col style={{ width: 20 + '%' }} />
+                        <col style={{ width: 10 + '%' }} />
+                    </colgroup>
                     <thead>
                         <tr>
                             <th>Time</th>
@@ -33,17 +43,22 @@ export class Trains extends Component {
                     <tbody>
                         {trains.services.map(service => {
                             return [
-                                <tr key={service.time}>
+                                <tr key={service.time} onClick={toggle}>
                                     <td>{service.time}</td>
                                     <td>{service.destination.name}</td>
                                     <td>{service.destination.duration}m</td>
                                     <td>{service.platform}</td>
                                     <td data-ontime={service.expected == "On time"}>{service.expected}</td>
                                     <td>{service.coaches}</td>
+                                </tr>,
+                                <tr className={this.state.isOpen ? "open" : "closed"}>
+                                    <td colSpan="3">{service.callingAt.map((callingPoint, index) => {
+                                        return <p key={index}>{callingPoint}</p>
+                                    })}
+                                    </td>
+                                    <td colSpan="3">{service.lastReport}</td>
                                 </tr>
-                                // <tr key={service.time + "-callingat"}>
-                                //     <td colSpan="5"><div className="marquee"><p>{service.callingAt.join(' ')}</p></div></td>
-                                // </tr>
+
                             ];
                         }
                         )}
